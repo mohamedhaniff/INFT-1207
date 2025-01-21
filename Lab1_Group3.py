1af06fdb6abebd301fce9f8466fc0d90907756fb
Beatlemaniac-JoJo <joel.gabud@dcmail.ca>
2025-01-21 12:22:36 -0500

    Update Lab1_Group3.py

--- Lab1_Group3.py
+++ Lab1_Group3.py
@@ -3,6 +3,7 @@ import  string
 total_length  = int(input("Enter the total Length of the password  "))
     if total_length < 8:
         print("Password must be at least 8 characters")
+        return total_length
 num_letters  = int(input("How many letters do you want "))
 for x in  range (0, num_letters):
     random_letter = random.choice(string.ascii_letters)
@@ -20,3 +21,6 @@ num_special_characters   = int( input("How many special characters do you want "
 for x in  range (0, num_special_characters ):
     random_special = random.choice(string.punctuation)
     print(f"Random special character: {random_special}")
+
+password_output = output(random_letter, random_digit, random_special)
+print(password_output)
